13fad5d96e24e2ebeff147131648545abf0c1380
Joseph Dayo <joseph.dayo@gmail.com>
2013-04-16 18:56:45 +0800

    OISEMS Node Server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oisems::Application.config.secret_token = '7386a3560db437a58c6fd53a8b31ebd6192e30d99f02c7891a608c755e7b47048c7635bd4becdac9eb1eb3753ce37690b5b3cb958726bd187c447d952eb6daa9'
